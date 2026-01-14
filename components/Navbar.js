4608fc07e03470eae5d68161b632978c61479cee
sxany <putuprawira425@gmail.com>
2026-01-14 13:20:13 +0800

    #

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,11 @@ import '../src/style.css';
 
 export function Navbar() {
   return `
-    
+    <nav class="nav">
+      <div class="mx-auto flex items-center">
+        <img class='cursor-pointer flex' src='./pict/1.jp'></img>
+      </div>
 
+    </nav>
   `;
 }
