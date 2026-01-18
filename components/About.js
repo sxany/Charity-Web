c53c328d52b1b61dac99650e17bb6e4b7a0cd9ac
sxany <97228203+sxany@users.noreply.github.com>
2026-01-19 01:17:07 +0800

    penambahan about

--- components/About.js
+++ components/About.js
@@ -1,7 +1,26 @@
 export function About() {
   return `
-    <div class=" w-full sm:w-3/4 mx-auto mt-4 px-4 sm:px-0">
-    <h1>About Us</h1>
+
+    <h1 class="mt-5 text-center text-base font-medium text-heading sm:text-2xl">
+      About Us
+    </h1>
+    <div class=" sm:w-3/4 mx-auto mt-4 px-4 ">
+
+     <div class="grid grid-cols-1 md:grid-cols-2 gap-10">
+    <h1 class="text-base font-medium text-heading sm:text-2xl">
+      AyoBantu adalah platform penggalangan dana online yang
+      didedikasikan untuk membantu individu dan komunitas yang membutuhkan
+      bantuan. Misi kami adalah memudahkan orang untuk berdonasi dan
+      berkontribusi pada berbagai tujuan kemanusiaan, sosial, dan lingkungan.
+    </h1>
+    <div class="grid-cols-1 md:grid-cols-2 ">
+          <img 
+            src="\./pict/1.png" 
+            alt="About AyoBantu"
+            class="w-[100%] max-w-md rounded-xl shadow-lg h-[500px] object-cover "
+          />
+        </div>
+    </div>
     </div>
     `
 }   
