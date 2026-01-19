4fb0836d48f133431002e24f9843ed5151737693
RyuBeast <senkuu41@gmail.com>
2026-01-19 22:07:50 +0800

    fix eror

--- components/Main.js
+++ components/Main.js
@@ -1,4 +1,4 @@
-import { Navbar } from './navbar'
+import { Navbar } from './Navbar'
 import { Jumbotron } from './Jumbotron'
 import { Application } from './Application'
 import { Cardfav } from './Cardfav'
