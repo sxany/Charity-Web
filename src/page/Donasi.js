ed81be76dad66dc50e0a46a71b8fe30c3c2ab4aa
sxany <putuprawira425@gmail.com>
2026-01-20 21:46:54 +0800

    #

--- src/page/Donasi.js
+++ src/page/Donasi.js
@@ -1,5 +1,4 @@
 export function Donasi() {
-    // tunggu DOM dirender
     setTimeout(() => {
         const form = document.getElementById("formDonasi");
         const message = document.getElementById("donasiMessage");
